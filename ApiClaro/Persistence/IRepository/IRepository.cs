62053188405e9d6cd4f86426ae64903a60565352
Josue Pujols <josuepujols20@icloud.com>
2022-03-21 19:58:05 -0400

    Initial commit

---
+++ ApiClaro/Persistence/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.IRepository
+{
+    //This is a generic repository to interactuate with each entity
+    interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(Guid Id);
+        Task<bool> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(Guid Id);
+        Task<bool> CommitChanges();
+    }
+}
---
+++ ApiClaro/Persistence/Repository/Repository.cs
@@ -0,0 +1,14 @@
+﻿using Persistence.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repository
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+
+    }
+}
